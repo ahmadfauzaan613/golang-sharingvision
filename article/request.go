81cb494495031f289c7bec7331ff5630e11223de
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2021-09-26 02:08:15 +0700

    update
    update

--- article/request.go
+++ article/request.go
@@ -1,8 +1,8 @@
 package article
 
 type ArticleRequest struct {
-	Title    string `json:"title" binding:"required"`
-	Content  string `json:"content" binding:"required"`
-	Category string `json:"category" binding:"required"`
+	Title    string `json:"title" binding:"required, min=20" `
+	Content  string `json:"content" binding:"required min=200"`
+	Category string `json:"category" binding:"required min=3"`
 	Status   string `json:"status" binding:"required"`
 }
